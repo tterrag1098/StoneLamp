e21fc400b5d319bbdd9802350930661d9e2db076
tterrag1098 <tterrag1098@gmail.com>
2013-11-14 02:01:28 -0500

    Proxy package name fix.

--- Stone Lamp/com/tterrag/stoneLamp/lib/Reference.java
+++ Stone Lamp/com/tterrag/stoneLamp/lib/Reference.java
@@ -4,9 +4,9 @@ public class Reference {
 
 	public static final String MOD_ID = "stoneLamp";
 	public static final String MOD_NAME = "Stone Lamp";
-	public static final String VERSION = "1.5";
-	public static final String CLIENT_PROXY_CLASS = "com.tterrag.akivarMod.proxy.ClientProxy";
-	public static final String SERVER_PROXY_CLASS = "com.tterrag.akivarMod.proxy.CommonProxy";
+	public static final String VERSION = "1.0.2";
+	public static final String CLIENT_PROXY_CLASS = "tterrag.stoneLamp.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "tterrag.stoneLamp.proxy.CommonProxy";
 	
 	public static final String TAB_NAME = "stoneLampTab";
 	public static final String TAB_LOC_NAME = "Stone Lamps"; 
