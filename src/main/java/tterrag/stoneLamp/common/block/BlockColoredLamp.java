d64ceb40550af48ead527aa952b2b14567e66cbb
tterrag1098 <tterrag1098@gmail.com>
2014-03-11 03:35:09 -0400

    Cleanup.

--- src/main/java/tterrag/stoneLamp/common/block/BlockColoredLamp.java
+++ src/main/java/tterrag/stoneLamp/common/block/BlockColoredLamp.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import tterrag.stoneLamp.common.config.ConfigKeys;
